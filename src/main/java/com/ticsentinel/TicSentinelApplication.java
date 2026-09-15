e5a77bd7cdfd18d0c6d93c16896dceb366b3b7ae
joseetpp <josetpayestas@gmail.com>
2026-09-15 14:23:22 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/ticsentinel/TicSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.ticsentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicSentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicSentinelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticsentinel/TicSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticsentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicSentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
