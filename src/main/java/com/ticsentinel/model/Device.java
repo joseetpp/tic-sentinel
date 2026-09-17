a46f7504d940298b580fa7968b91deb6b2b449bd
joseetpp <josetpayestas@gmail.com>
2026-09-17 13:45:10 +0200

    Add device model

--- src/main/java/com/ticsentinel/model/Device.java
+++ src/main/java/com/ticsentinel/model/Device.java
@@ -1,4 +1,79 @@
 package com.ticsentinel.model;
 
 public class Device {
+    private Long id;
+    private String name;
+    private String type;
+    private String brand;
+    private String model;
+    private String operatingSystem;
+    private String serialNumber;
+    private String Status;
+
+    public Device() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
 }
